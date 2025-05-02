6ec1111a40f9534a9b9b17bf4bae50a0e543a6ac
mschichuang <mschichuang@icloud.com>
2025-05-02 22:09:26 +0800

    😔

--- Assets/Emily/Scripts/TransferCoins.cs
+++ Assets/Emily/Scripts/TransferCoins.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
 using SpatialSys.UnitySDK;
 
 public class TransferCoins : MonoBehaviour
@@ -8,9 +10,40 @@ public class TransferCoins : MonoBehaviour
     public void OnTransferTriggered()
     {
         transferDiamond.SetActive(false);
-
         string playerName = SpatialBridge.actorService.localActor.displayName.Split(' ')[1];
+        CheckIsLeader(playerName);
+    }
 
-        bool isLeader = false; // ← 之後改成從表單取得
+    private async void CheckIsLeader(string name)
+    {
+        string url = $"https://script.google.com/macros/s/AKfycbwVKSdMOP-b8GEt_v1DQCtNXMes86mWpVae0BndvF6KPo9CHg87b2sfkXA3YdkM_ZNZ/exec?name={name}";
+
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            var op = request.SendWebRequest();
+            while (!op.isDone)
+                await Task.Yield();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                string json = request.downloadHandler.text;
+                bool isLeader = JsonUtility.FromJson<IsLeaderResponse>(json).isLeader;
+
+                if (isLeader)
+                {
+                    Debug.Log("✅ 是組長");
+                }
+                else
+                {
+                    ulong currentBalance = SpatialBridge.inventoryService.worldCurrencyBalance;
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    private class IsLeaderResponse
+    {
+        public bool isLeader;
     }
 }
