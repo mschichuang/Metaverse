c895398fe62ef7912bf20344d72717d0fae8a98a
Cool0806 <zhengangel930806@gmail.com>
2025-05-15 20:46:06 +0800

    專題 錢幣指引指引
    專題 錢幣指引指引

--- Assets/Cool/Scripts/New Folder/CoinCollect.cs
+++ Assets/Cool/Scripts/New Folder/CoinCollect.cs
@@ -3,25 +3,34 @@ using SpatialSys.UnitySDK;
 
 public class CoinCollect : MonoBehaviour
 {
-    public float rotateSpeed = 90f; // 每秒旋轉角度
-    public ulong rewardAmount = 1000; // 獎勵金幣數量
+    public float rotateSpeed = 90f; // 每秒旋轉角度（繞 Z 軸）
+    public ulong rewardAmount = 1000; // 獎勵金額
+
+    [HideInInspector]
+    public CoinLineConnector connectedLine; // 由 CoinLineConnector 設定
 
     void Update()
     {
-        // 讓金幣繞 Y 軸旋轉（像立起來的硬幣轉動）
+        // 讓金幣繞 Z 軸旋轉（像立起來的硬幣轉動）
         transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // 印出誰碰到金幣，幫助除錯
-        Debug.Log("碰到金幣的物體名稱：" + other.gameObject.name);
-
-        // 如果是玩家碰到，就給錢並刪除金幣
+        // 如果是 Avatar 玩家碰到
         if (other.gameObject.name.Contains("Avatar"))
         {
-            Debug.Log("玩家觸發金幣，發送獎勵！");
+            Debug.Log($"玩家觸發金幣 {gameObject.name}，發送獎勵！");
             SpatialBridge.inventoryService.AwardWorldCurrency(rewardAmount);
+
+            // 通知線條（如果有）
+            if (connectedLine != null)
+            {
+                Debug.Log("通知線條其中一端金幣被收集！");
+                connectedLine.NotifyCoinCollected(this);
+            }
+
+            // 銅板消失
             Destroy(gameObject);
         }
     }
