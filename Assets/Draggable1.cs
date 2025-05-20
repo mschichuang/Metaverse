d1dd30abb768be6c2387b3558e60121c2a88fd09
lincy930112 <0112ariel@gmail.com>
2025-05-20 19:52:19 +0800

    更

--- Assets/Draggable1.cs
+++ Assets/Draggable1.cs
@@ -1,14 +1,13 @@
-﻿using UnityEngine;  // ✅ using 放在最前面！
+﻿using UnityEngine;
 
 public class Draggable1 : MonoBehaviour
 {
     private Camera cam;
     private bool isDragging = false;
-    private float distance;
+    private float distanceToCamera;
 
     void Start()
     {
-        // 確保從場景中找到 Camera
         cam = FindObjectOfType<Camera>();
     }
 
@@ -16,32 +15,28 @@ public class Draggable1 : MonoBehaviour
     {
         if (cam == null) return;
 
-        // 點擊開始拖曳
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                if (hit.collider.gameObject == gameObject)
+                if (hit.collider != null && hit.collider.gameObject == gameObject)
                 {
                     isDragging = true;
-                    distance = Vector3.Distance(cam.transform.position, transform.position);
+                    distanceToCamera = Vector3.Distance(cam.transform.position, transform.position);
                 }
             }
         }
 
-        // 拖曳中
-        if (isDragging && Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && isDragging)
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            Vector3 point = ray.GetPoint(distance);
+            Vector3 point = ray.GetPoint(distanceToCamera);
             transform.position = point;
         }
 
-        // 停止拖曳
         if (Input.GetMouseButtonUp(0))
         {
             isDragging = false;
         }
     }
-}
