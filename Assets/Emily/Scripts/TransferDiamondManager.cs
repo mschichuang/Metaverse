869fdb1bcc247ac8409c6d7d5bc74b3d70d70b60
mschichuang <mschichuang@icloud.com>
2025-06-06 11:46:55 +0800

    😀

---
+++ Assets/Emily/Scripts/TransferDiamondManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using SpatialSys.UnitySDK;
+using System.Threading.Tasks;
+
+public class TransferDiamondManager : MonoBehaviour
+{
+    private string playerName;
+
+    private async void Start()
+    {
+        playerName = SpatialBridge.actorService.localActor.displayName.Split(' ')[1];
+
+        await CheckIsLeader();
+    }
+
+    private async Task CheckIsLeader()
+    {
+        string url = $"https://script.google.com/macros/s/AKfycbyQD56ArfGkOuYfa-RRqYFPbSDLbSdsU98UWw86XBcjPaQ4NJ9GhegNnocDrX5hdlfZ/exec?name={playerName}";
+
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            request.SendWebRequest();
+            while (!request.isDone)
+                await Task.Yield();
+
+            string json = request.downloadHandler.text;
+            LeaderResponse data = JsonUtility.FromJson<LeaderResponse>(json);
+
+            if (data.isLeader == "Y")
+            {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+
+    [System.Serializable]
+    private class LeaderResponse
+    {
+        public string isLeader;
+    }
+}
