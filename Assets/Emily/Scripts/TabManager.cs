eb7d33150d602f70d3e9788f8e99d107bac97679
mschichuang <mschichuang@icloud.com>
2025-07-04 13:46:34 +0800

    🌶️

--- Assets/Emily/Scripts/TabManager.cs
+++ Assets/Emily/Scripts/TabManager.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TabManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] pages;
 
-    // Update is called once per frame
-    void Update()
+    public void ShowPage(int index)
     {
-        
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(i == index);
+        }
     }
 }
