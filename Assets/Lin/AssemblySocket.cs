d7d68e89b6a71f8531346a92e1f44064b15f47ba
lincy930112 <0112ariel@gmail.com>
2025-11-06 19:52:13 +0800

    更

--- Assets/Lin/AssemblySocket.cs
+++ Assets/Lin/AssemblySocket.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 public class AssemblySocket : MonoBehaviour
 {
-    [Tooltip("允許的零件 ID（相同 string 才能組裝）")]
     public string allowedPartID;
 
-    [Tooltip("放入後是否固定為子物件")]
     public bool attachAsChild = true;
 
-    [Tooltip("是否在放入後鎖定位置與旋轉")]
     public bool lockTransform = true;
 
-    // 可視化用：畫出插槽框
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
