134b2c5c04092df523a98024ff2e4346e0209dd9
Cool0806 <zhengangel930806@gmail.com>
2025-11-13 20:31:47 +0800

    132
    312

---
+++ Assets/Cool/Scripts/202511/NPCTriggerPoint.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTriggerPoint : MonoBehaviour
+{
+    public string pointName; // 觸發點名稱
+    private bool isVisited = false; // 是否已探索
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isVisited)
+        {
+            isVisited = true;
+            NPCTriggerTracker tracker = FindObjectOfType<NPCTriggerTracker>(); // 找到 NPC
+            if (tracker != null)
+            {
+                tracker.MarkTriggerAsVisited(this); // 通知 NPC 更新狀態
+            }
+        }
+    }
+
+    public bool GetVisited()
+    {
+        return isVisited;
+    }
+}
